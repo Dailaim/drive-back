0696bacaaf56241d52e5711ee2efde4b56021d9c
Daizaikun <leo-im@hotmail.com>
2023-06-01 22:01:34 -0500

    add lib and checker envs

---
+++ app/lib/checkEnv.go
@@ -0,0 +1,13 @@
+package lib
+
+import "os"
+
+
+// Comprueba variables de entorno
+func CheckEnv(env string, def string) string {
+	variable := os.Getenv(env)
+	if len(variable) == 0 {
+		return def
+	}
+	return variable
+}
