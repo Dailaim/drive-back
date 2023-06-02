f4f8e53d263e1d9876d42f5f370f633f06dbe4a3
Daizaikun <leo-im@hotmail.com>
2023-06-01 22:02:24 -0500

    add rider

---
+++ app/modules/rider/model.go
@@ -0,0 +1,15 @@
+package rider
+
+import (
+	"github.com/Daizaikun/wompi-back/app/modules/ride"
+	"gorm.io/gorm"
+)
+
+type Model struct {
+	gorm.Model
+	Name          string
+	Email         string `gorm:"type:varchar(100);unique_index"`
+	Password      string
+	PaymentSource string
+	Rides         []ride.Model `gorm:"foreignKey:RiderID"`
+}
