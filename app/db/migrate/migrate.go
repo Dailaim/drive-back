a7b759e52b5ed802b6af1d633774f802e6440c7f
Daizaikun <leo-im@hotmail.com>
2023-06-01 22:02:47 -0500

    change migrate with modules

--- app/db/migrate/migrate.go
+++ app/db/migrate/migrate.go
@@ -1,14 +1,18 @@
 package migrate
 
 import (
-
+	"github.com/Daizaikun/wompi-back/app/modules/driver"
+	"github.com/Daizaikun/wompi-back/app/modules/ride"
+	"github.com/Daizaikun/wompi-back/app/modules/rider"
 	"gorm.io/gorm"
 )
 
 func New(DB *gorm.DB) error {
 
 	err := DB.AutoMigrate(
-
+		&driver.Model{},
+		&ride.Model{},
+		&rider.Model{},
 	)
 
 	return err
