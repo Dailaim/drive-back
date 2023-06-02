586babacba5a6bfd6a220e1abc2a4dc67ef93e04
Daizaikun <leo-im@hotmail.com>
2023-06-01 21:57:31 -0500

    add dependes gorm, fiber and dotenv

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"github.com/Daizaikun/wompi-back/app/config"
+	"github.com/Daizaikun/wompi-back/app/db"
+	"github.com/joho/godotenv"
+)
 
+func init() {
+	godotenv.Load()
+}
+
+func init() {
+	db.Connect()
+}
+
+func main() {
+	config.Run().Listen(":8080")
 }
