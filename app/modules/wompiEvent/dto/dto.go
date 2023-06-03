2b1e0f918f5b5d604080a4065b194876d8de33f7
Daizaikun <leo-im@hotmail.com>
2023-06-02 20:41:15 -0500

    add integration wompi

---
+++ app/modules/wompiEvent/dto/dto.go
@@ -0,0 +1,35 @@
+package dto
+
+import "time"
+
+type Transaction struct {
+	ID                string `json:"id"`
+	AmountInCents     int    `json:"amount_in_cents"`
+	Reference         string `json:"reference"`
+	CustomerEmail     string `json:"customer_email"`
+	Currency          string `json:"currency"`
+	PaymentMethodType string `json:"payment_method_type"`
+	RedirectURL       string `json:"redirect_url"`
+	Status            string `json:"status"`
+	ShippingAddress   string `json:"shipping_address"`
+	PaymentLinkID     string `json:"payment_link_id"`
+	PaymentSourceID   string `json:"payment_source_id"`
+}
+
+type Signature struct {
+	Properties []string `json:"properties"`
+	Checksum   string   `json:"checksum"`
+}
+
+type Event struct {
+	Event       string    `json:"event"`
+	Data        Data      `json:"data"`
+	Environment string    `json:"environment"`
+	Signature   Signature `json:"signature"`
+	Timestamp   int64     `json:"timestamp"`
+	SentAt      time.Time `json:"sent_at"`
+}
+
+type Data struct {
+	Transaction Transaction `json:"transaction"`
+}
